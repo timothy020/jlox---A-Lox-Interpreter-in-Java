d2ca39b62c0b919f2fecfbe23c052bd1d8ee57eb
timothy020 <1439856981@qq.com>
2025-03-08 14:06:23 +0800

    添加Token类型

--- src/TokenType.java
+++ src/TokenType.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public enum TokenType {
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
+public enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN,    // ( )
+    LEFT_BRACE, RIGHT_BRACE,    // { }
+    COMMA, DOT, MINUS, PLUS,    // , . - +
+    SEMICOLON, SLASH, STAR,     // ; / *
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,       // !  !=
+    EQUAL, EQUAL_EQUAL,     // =  ==
+    GREATER, GREATER_EQUAL, // >  >=
+    LESS, LESS_EQUAL,       // >  >=
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
 }
