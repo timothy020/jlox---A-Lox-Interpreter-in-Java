a1635fd9d91ffe0e2566f7eb21d80a798ddbd943
timothy020 <1439856981@qq.com>
2025-03-08 14:14:15 +0800

    feat: 添加 Token 类
    - 新增 Token 类，用于表示词法分析后的标记
    - Token 类包含类型、词法、字面值和行号等信息
    - 在 Lox 类中添加了对 Token 类的引用

--- src/Lox.java
+++ src/Lox.java
@@ -3,11 +3,11 @@ import jdk.nashorn.internal.parser.Token;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-
 
 public class Lox {
     static boolean hadError = false;
