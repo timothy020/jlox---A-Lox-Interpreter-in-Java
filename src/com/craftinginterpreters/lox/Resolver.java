a9005d4d2479a2b33074b28c69fb2fdbc46819d2
timothy020 <1439856981@qq.com>
2025-03-18 11:43:21 +0800

    feat(Resolver): 增加 super 关键字错误处理

--- src/com/craftinginterpreters/lox/Resolver.java
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -14,7 +14,7 @@ public class Resolver implements
     private ClassType currentClass = ClassType.NONE;
 
     private enum FunctionType { NONE, FUNCTION, INITIALIZER, METHOD}
-    private enum ClassType    { NONE, CLASS }
+    private enum ClassType    { NONE, CLASS, SUBCLASS }
 
     Resolver(Interpreter interpreter) {
         this.interpreter = interpreter;
@@ -101,6 +101,7 @@ public class Resolver implements
 
         // 处理父类: this 的外层再包裹一个 super environment
         if(stmt.superclass != null){
+            currentClass = ClassType.SUBCLASS;
             resolve(stmt.superclass);
         }
         if (stmt.superclass != null) {
@@ -139,6 +140,14 @@ public class Resolver implements
     }
     @Override
     public Void visitSuperExpr(Expr.Super expr) {
+        if(currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword,
+                    "Can't use 'super' outside of a class.");
+        } else if (currentClass != ClassType.SUBCLASS) {
+            Lox.error(expr.keyword,
+                    "Can't use 'super' in a class with no superclass.");
+        }
+
         resolveLocal(expr, expr.keyword);
         return null;
     }
